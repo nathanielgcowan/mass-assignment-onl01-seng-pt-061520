566d3b284388b3648f67ce2ee6467fa86d029d38
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-27 01:00:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   #your code here
-    attr_accessor :name, :birthday, :hair_coolor, :user_name
+    attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
  
   def initialize(user_name:, name:, age:, location:)
     @user_name = user_name
